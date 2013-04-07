3c5227a4993e2cde88ecf0985870a90c2e3bbfff
Jonathan Wesley Stone <js@piratejon.com>
2013-04-07 14:15:04 -0500

    switching from example

---
+++ src/ds.h
@@ -0,0 +1,7 @@
+#ifndef _DS_H
+#define _DS_H
+
+int n_plus_two ( int );
+
+#endif // _DS_H
+
---
+++ src/ds_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "ds.h"
+
+int main ( int arfc, char ** arfv )
+{
+
+  int x = 9;
+
+  printf("%d\n", n_plus_two ( x ) );
+
+  return 0;
+}
+
