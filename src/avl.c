df1e2abb0b116f5fe7f0887a6c21a64da1aad930
Jonathan Wesley Stone <js@piratejon.com>
2013-04-13 04:56:59 -0500

    tracking avl.c now. also, trivially passing test

---
+++ src/avl.c
@@ -0,0 +1,9 @@
+
+#include <sys/types.h>
+
+#include "avl.h"
+
+AVLTree * init_avl_tree ( size_t bucket_size ) {
+  return 99;
+}
+
--- src/avl.h
+++ src/avl.h
@@ -1,4 +1,10 @@
 #ifndef _JS_AVL_H
 
+#include <sys/types.h>
+
+typedef struct _tag_avltree {
+  size_t bucket_size;
+} AVLTree;
+
 #endif // _JS_AVL_H
 
