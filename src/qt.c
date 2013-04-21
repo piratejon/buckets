2447f7b2fa11147dfb250706c045391105f7b9ef
Jonathan Wesley Stone <js@piratejon.com>
2013-04-20 22:35:57 -0500

    passing test

--- src/qt.c
+++ src/qt.c
@@ -1,11 +1,27 @@
 
+#include <stdlib.h>
+
 #include "qt.h"
 
 QuadTree * init_quadtree() {
-  return malloc(1);;
+  QuadTree * qt = malloc(sizeof(QuadTree));
+  qt->root = malloc(sizeof(QTNode));
+  qt->root->nw = qt->root->ne = qt->root->sw = qt->root->se = NULL;
+  qt->root->children = NULL;
+  return qt;
+}
+
+void destroy_quadtree_nodes(QTNode * qt) {
+  if (qt->nw) destroy_quadtree_nodes(qt->nw);
+  if (qt->ne) destroy_quadtree_nodes(qt->ne);
+  if (qt->sw) destroy_quadtree_nodes(qt->sw);
+  if (qt->se) destroy_quadtree_nodes(qt->se);
+  if (qt->children) destroy_avl_tree(qt->children);
+  free(qt);
 }
 
 void destroy_quadtree ( QuadTree * qt ) {
+  destroy_quadtree_nodes(qt->root);
   free(qt);
 }
 
