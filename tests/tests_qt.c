ea4ae5b052ce049a48f7eed9aa133f6d3ef22773
Jonathan Wesley Stone <js@piratejon.com>
2013-04-20 22:13:34 -0500

    tests fails because code does not exist yet

--- tests/tests_qt.c
+++ tests/tests_qt.c
@@ -10,9 +10,18 @@ void sanity_check_zero ( void )
   ASSERT ( 0 == 0, "Zero failed to be equal to zero." );
 }
 
+void test_initialize_quadtree ( void )
+{
+  QuadTree * qt = init_quadtree();
+
+  ASSERT ( qt, "NULL quadtree");
+
+  destroy_quadtree(qt);
+}
+
 void do_tests ( void )
 {
   TEST ( sanity_check_zero );
+  TEST ( test_initialize_quadtree );
 }
-
 
